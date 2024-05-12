28801643155cd10a510cfc032c02e7bd71115cae
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-05-13 00:34:05 +0200

    [rootfs-templates] New Droidian: switch to snapshots
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Remove extrepos
-rm -f /etc/apt/sources.list.d/extrepo_droidian.sources
-rm -f /etc/apt/sources.list.d/extrepo_mobian.sources
-rm -f /var/lib/extrepo/keys/hybris-mobian.asc
-rm -f /var/lib/extrepo/keys/mobian.asc
+rm -f /etc/apt/trusted.gpg.d/droidian-bootstrap.gpg
 
 # Nuke /etc/apt/sources.list
 > /etc/apt/sources.list
